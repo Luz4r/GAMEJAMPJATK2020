d06a991d721c5a62ec42080622b6d6e2375e24ed
advmczvk <kuba.adamczyk99@gmail.com>
2020-11-01 06:36:22 +0100

    add intro

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class Intro : MonoBehaviour
+{
+    private void Start()
+    {
+        StartCoroutine(Wait());
+    }
+
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(12);
+        SceneManager.LoadScene("MenuScene");
+    }
+}
