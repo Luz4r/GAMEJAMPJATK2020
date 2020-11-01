7dfa998c5e7b6c26d328046f2f6646db9dbb3879
advmczvk <kuba.adamczyk99@gmail.com>
2020-11-01 03:52:15 +0100

    added pause menu

---
+++ Assets/Scripts/Menu/Pause.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject pause;
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
